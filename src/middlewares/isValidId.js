93c006af6563919c472d5667781403e45c256c5d
MaX <maksim.shepeta@gmail.com>
2025-04-13 17:18:46 +0200

    chore: add understandful validId error message

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -4,7 +4,7 @@ import { isValidObjectId } from 'mongoose';
 export const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    throw createHttpError(400, 'Bad request');
+    throw createHttpError(400, `Contact with id ${id} is not found`);
   }
 
   next();
